73226182e06e1165c2888c6abd2bebc9cc0e40b8
Nathan Loveless <nathansl2003@yahoo.com>
2019-10-11 22:12:08 -0400

    Working on MVP, starting Character card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
+import CharacterCard from './components/CharacterCard';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
+      <Route exact path="/" component={CharacterList} />
+  <Route path="/item-list/:id" component={CharacterCard} />
     </main>
   );
+
+
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,47 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characterList, setCharacterList] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const characterList = await axios.get(
+          "https://rickandmortyapi.com/api/character/"
+        );
+        console.log(characterList.data.results);
+        setCharacterList(characterList.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+  <section className="character-list">
+      {characterList.map(character => {
+        return (
+          // <CharacterCard
+          //   name={character.name}
+          //   status={character.status}
+          //   species={character.species}
+          //   type={character.type}
+          //   />
+          <div>
+            <h2>{character.name}</h2>
+            <img src={character.image}></img>
+          </div>
+        );
+      })}
     </section>
   );
 }
